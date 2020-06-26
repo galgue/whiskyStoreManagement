69e5602bb74aa90d842892647226e6977ade79fc
bturtu405 <54838965+bturtu405@users.noreply.github.com>
2020-06-26 19:47:36 +0300

    Update Table.tsx
    update

--- frontend/src/Components/Table.tsx
+++ frontend/src/Components/Table.tsx
@@ -3,10 +3,16 @@ import MaterialTable, { Column, Options, Action } from 'material-table';
 
 
 interface Row {
-    name: string;
-    surname: string;
-    birthYear: number;
-    birthCity: number;
+    id: number;
+    barrelType: number;
+    chainName: number;
+    timeInBarrel: number;
+    liquidQuantity: number;
+    previousId: number;
+    alcoholPercentage: number;
+    distillateType: number;
+    owner: string;
+    location: string;
 }
 
 interface TableState {
@@ -19,35 +25,94 @@ export const BatchesTable = () => {
     const [state, setState] = React.useState<TableState>({
         columns: [
             {
-                title: 'שם', field: 'name', 
+                title: 'מק"ט', field: 'id', type: 'numeric',
                 cellStyle: {
                     textAlign: 'right'
                 }
             },
-            { title: 'שם משפחה', field: 'surname', 
-            cellStyle: {
-                textAlign: 'right'
-            } },
-            { title: 'שנת לידה', field: 'birthYear', type: 'numeric', 
-            cellStyle: {
-                textAlign: 'right'
-            } },
             {
-                title: 'מקום לידה',
-                field: 'birthCity',
-                lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' }, 
+                title: 'סוג חבית', field: 'barrelType',
+                cellStyle: {
+                    textAlign: 'right'
+                },
+                lookup: { 1: 'אלון', 2: 'cherry' },
+            },
+            {
+                title: 'שם שרשרת', field: 'chainName',
+                cellStyle: {
+                    textAlign: 'right'
+                },
+                lookup: { 1: 'סטנדרטי', 2: 'יישון ארוך' },
+            },
+            {
+                title: 'תקופת יישון', field: 'timeInBarrel', type: 'numeric',
+                cellStyle: {
+                    textAlign: 'right'
+                }
+            },
+            {
+                title: 'כמות נוזל במילוי', field: 'liquidQuantity', type: 'numeric',
+                cellStyle: {
+                    textAlign: 'right'
+                }
+            },
+            {
+                title: 'מק"ט אצוות חבית קודמת', field: 'previousId',
+                cellStyle: {
+                    textAlign: 'right'
+                },
+                lookup: {111111:111111,222222:222222},
+            },
+            {
+                title: 'אחוז אלכוהול במילוי', field: 'alcoholPercentage', type: 'numeric',
+                cellStyle: {
+                    textAlign: 'right'
+                }
+            },
+            {
+                title: 'סוג תזקיק', field: 'distillateType',
+                cellStyle: {
+                    textAlign: 'right'
+                },
+                lookup: {1:'ניו מייק סינגל מאלט',2:'גין לבנטיני',3:'סינגל מאלט קאלסי',4:'ליקר רוטס'},
+            },
+            {
+                title: 'בעלות', field:'owner',
+                cellStyle: {
+                    textAlign: 'right'
+                }
+            },
+            {
+                title: 'מיקום חבית במשרד', field:'location',
                 cellStyle: {
                     textAlign: 'right'
                 }
             },
         ],
         data: [
-            { name: 'יוסי', surname: 'שמעון', birthYear: 1987, birthCity: 63 },
             {
-                name: 'עזרא',
-                surname: 'חנן',
-                birthYear: 2017,
-                birthCity: 34,
+                id: 123456,
+                barrelType: 2,
+                chainName: 2,
+                timeInBarrel: 345,
+                liquidQuantity: 120,
+                alcoholPercentage: 35,
+                location: 'אזור א',
+                owner: 'יוסי דביר',
+                previousId: 222222,
+                distillateType: 2
+            },
+            {
+                id: 222222,
+                barrelType: 1,
+                chainName: 1,
+                timeInBarrel: 123,
+                liquidQuantity: 230,
+                alcoholPercentage: 40,
+                location: 'אזור א',
+                owner: 'יוסי דביר',
+                previousId: 123456,
+                distillateType: 1
             },
         ],
     });
@@ -59,18 +124,28 @@ export const BatchesTable = () => {
 
     return (
         <MaterialTable
-            title="אצוות חבית"
-            style={{ direction: 'rtl', textAlign: 'right' }}
+            title='אצוות חביות'
+            style={{ direction: 'rtl'}}
             localization={{
                 toolbar: {
-                    searchPlaceholder: 'חפש'
+                    searchPlaceholder: 'חפש',
                 },
-                header:{
-                    actions:'פעולות'
+                body:{
+                    addTooltip: 'הוסף',
+                  deleteTooltip: 'מחק',
+                  editTooltip: 'ערוך',
+                  editRow: {
+                    deleteText: 'האם את\\ה בטוח\\ה?',
+                    cancelTooltip: 'בטל',
+                    saveTooltip: 'שמור',
+                  },
                 },
-                pagination:{
-                    firstAriaLabel:'שורות',
-                    firstTooltip:'עמוד ראשון',
+                header: {
+                    actions: 'פעולות'
+                },
+                pagination: {
+                    firstAriaLabel: 'שורות',
+                    firstTooltip: 'עמוד ראשון',
                     labelDisplayedRows: '{from}-{to} מתוך {count}',
                     labelRowsPerPage: 'שורות בכל עמוד',
                     labelRowsSelect: 'שורות',
